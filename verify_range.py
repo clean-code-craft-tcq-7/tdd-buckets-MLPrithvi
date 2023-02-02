42d3f4f8a2b3806a17757f26b6e096d1db1e4b0d
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2023-02-02 23:20:01 +0530

    Update verify_range.py

--- verify_range.py
+++ verify_range.py
@@ -3,4 +3,5 @@ from range_identifier import range_detection
 if __name__ == '__main__':
     assert(range_detection([3,3,4,5,10,11,10,12], 3, 5) == 4)
     assert(range_detection([3,3,4,5,10,11,10,12], 10, 12) == 4)
-    print("Range & Readings are perfect")
+    print_output([3,3,4,5,10,11,10,12], 3, 5)
+    print_output([3,3,4,5,10,11,10,12], 10, 12)
